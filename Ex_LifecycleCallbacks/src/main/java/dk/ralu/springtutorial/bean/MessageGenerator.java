2897f4a8ed09c96748958e93c29f6f1e0562be6d
Rasmus Lund <rasmus@ralu.dk>
2017-04-17 16:23:14 +0200

    Modified Ex_LifecycleCallbacks to show that post construct is called AFTER injection has happened

--- Ex_LifecycleCallbacks/src/main/java/dk/ralu/springtutorial/bean/MessageGenerator.java
+++ Ex_LifecycleCallbacks/src/main/java/dk/ralu/springtutorial/bean/MessageGenerator.java
@@ -1,15 +1,31 @@
 package dk.ralu.springtutorial.bean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageGenerator {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageGenerator.class);
+
     @Autowired
     private RandomWordGenerator randomWordGenerator;
 
     public String generateMessage() {
         return randomWordGenerator.generateGreetingWord() + " " + randomWordGenerator.generateLocationWord();
+    }
+
+    @PostConstruct
+    private void onInit() {
+        LOGGER.info("MessageGenerator.onInit() was called (injection has happened? " + (randomWordGenerator != null) + ")");
+    }
+
+    @PreDestroy
+    private void onDestroy() {
+        LOGGER.info("MessageGenerator.onDestroy() was called");
     }
 }
--- Ex_LifecycleCallbacks/src/main/java/dk/ralu/springtutorial/bean/RandomWordGenerator.java
+++ Ex_LifecycleCallbacks/src/main/java/dk/ralu/springtutorial/bean/RandomWordGenerator.java
@@ -1,15 +1,9 @@
 package dk.ralu.springtutorial.bean;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 class RandomWordGenerator {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(RandomWordGenerator.class);
 
     String generateGreetingWord() {
         return Math.random() < 0.5 ? "Hi" : "Hello";
@@ -17,15 +11,5 @@ class RandomWordGenerator {
 
     String generateLocationWord() {
         return Math.random() < 0.5 ? "World" : "Universe";
-    }
-
-    @PostConstruct
-    private void onInit() {
-        LOGGER.info("RandomWordGenerator.onInit() was called");
-    }
-
-    @PreDestroy
-    private void onDestroy() {
-        LOGGER.info("RandomWordGenerator.onDestroy() was called");
     }
 }
