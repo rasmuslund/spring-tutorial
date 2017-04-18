b0eaad9d2290c8c13286a0cf2f19c6453fd4145f
Rasmus Lund <rasmus@ralu.dk>
2017-04-18 18:24:02 +0200

    Added comments to Ex_MinimalWithJavaConfiguration

--- Ex_MinimalWithJavaConfiguration/src/main/java/dk/ralu/springtutorial/bean/MessageGenerator.java
+++ Ex_MinimalWithJavaConfiguration/src/main/java/dk/ralu/springtutorial/bean/MessageGenerator.java
@@ -2,12 +2,12 @@ package dk.ralu.springtutorial.bean;
 
 public class MessageGenerator {
 
+    // This bean has a dependency on another bean
     private final RandomWordGenerator randomWordGenerator;
 
     public MessageGenerator(RandomWordGenerator randomWordGenerator) {
         this.randomWordGenerator = randomWordGenerator;
     }
-
 
     public String generateMessage() {
         return randomWordGenerator.generateGreetingWord() + " " + randomWordGenerator.generateLocationWord();
--- Ex_MinimalWithJavaConfiguration/src/main/java/dk/ralu/springtutorial/config/ApplicationConfiguration.java
+++ Ex_MinimalWithJavaConfiguration/src/main/java/dk/ralu/springtutorial/config/ApplicationConfiguration.java
@@ -5,15 +5,18 @@ import dk.ralu.springtutorial.bean.RandomWordGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// Signals that this class contains Spring configuration
 @Configuration
 public class ApplicationConfiguration {
 
+    // Defines a bean of the type RandomWordGenerator with the name randomWordGenerator (the name of the method)
     @Bean
     public RandomWordGenerator randomWordGenerator() {
         return new RandomWordGenerator();
     }
 
-    // Note that this bean needs a reference to the RandomWordGenerator bean, which means we add it as a parameter.
+    // Defines yet another bean, but this bean expects to get a reference to a RandomWordGenerator bean injected.
+    // With Java config we tell Spring that our beans expects an injection by declaring the dependencies as method parameters.
     // Spring will know that it has to create the RandomWordGenerator bean before creating this bean (calculates a dependency graph).
     @Bean
     public MessageGenerator messageGenerator(RandomWordGenerator randomWordGenerator) {
