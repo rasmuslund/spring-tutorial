e7b92c1ab6b62942feeff715bf5be583529f0bc8
Rasmus Lund <rasmus@ralu.dk>
2017-04-29 23:24:23 +0200

    In Main of Ex_SimpleSpringBootApp, removed unused @Bean method.

--- Ex_SimpleBootApp/src/main/java/dk/ralu/springtutorial/Main.java
+++ Ex_SimpleBootApp/src/main/java/dk/ralu/springtutorial/Main.java
@@ -2,17 +2,11 @@ package dk.ralu.springtutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-    }
-
-    @Bean
-    public String someString() {
-        return "Hello world";
     }
 }
