4e3e6f9d683bc0e206ba47b08e33d93ca82c7cdd
Rasmus Lund <rasmus@ralu.dk>
2017-05-06 16:34:28 +0200

    Added example of using @TestConfiguration to Ex_SimpleSpringBootApp

---
+++ Ex_SimpleBootApp/src/test/java/dk/ralu/springtutorial/beans/MessageGeneratorTest.java
@@ -0,0 +1,47 @@
+package dk.ralu.springtutorial.beans;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MessageGeneratorTest {
+
+    @Autowired
+    private MessageGenerator messageGenerator;
+
+    // Whatever beans this class defines, will be added to the beans that already exist
+    @TestConfiguration
+    public static class ExtraTestSpecificBeans {
+
+        @Primary // Now two beans of type RandomWordGenerator exist - marking one as primary means it will be the one that gets injected
+        @Bean
+        RandomWordGenerator fixedRandomWordGenerator() {
+            return new RandomWordGenerator() {
+
+                @Override
+                String generateGreetingWord() {
+                    return "GREETING_WORD";
+                }
+
+                @Override
+                String generateLocationWord() {
+                    return "LOCATION_WORD";
+                }
+            };
+        }
+
+    }
+
+    @Test
+    public void generateMessage() {
+        Assert.assertEquals("knud", messageGenerator.generateMessage());
+    }
+}
