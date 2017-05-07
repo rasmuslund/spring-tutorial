177782a54513dc66a5fc24bfaf9d27e3880673d8
Rasmus Lund <rasmus@ralu.dk>
2017-05-07 10:45:15 +0200

    Improved code comments in Ex_SimpleSpringBootApp

--- Ex_SimpleBootApp/src/test/java/dk/ralu/springtutorial/beans/MessageGeneratorTest.java
+++ Ex_SimpleBootApp/src/test/java/dk/ralu/springtutorial/beans/MessageGeneratorTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -21,7 +22,7 @@ public class MessageGeneratorTest {
     public static class ExtraTestSpecificBeans {
 
         // Now two beans of type RandomWordGenerator exist - marking one as primary means it will be the one that gets injected
-        // @Primary
+        @Primary
         @Bean
         RandomWordGenerator fixedRandomWordGenerator() {
             // This version of RandomWordGenerator returns fixed strings, to make testing of MessageGenerator easier
